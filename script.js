c009fb1125c9a6b8bc06a6a48196ed30815aec59
Sarthak Jain <93116216+SarthakJain29@users.noreply.github.com>
2025-07-16 13:30:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 const makeChange = (c) => {
-  // your name here
+  const change = {q:0, d:0, n:0, p:0};
+  while(c >= 25){
+	  change.q += 1;
+	  c -= 25;
+  }
+  while(c>=10){
+	  change.d += 1;
+	  c -= 10;
+  }
+  while(c>=5){
+	  change.n += 1;
+	  c -= 5;
+  }
+  while(c>=1){
+	  change.p += 1;
+	  c -= 1;
+  }
+  return change;
 };
 
 // Do not the change the code below
